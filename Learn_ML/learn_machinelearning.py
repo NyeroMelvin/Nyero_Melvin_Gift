a1cd671a5c6879ee7e01d7ba9d0ef27ad60de8e3
Melvin <melvinnyero1@gmail.com>
2025-06-16 20:51:49 +0300

    Convert Learn_ML from submodule/gitlink to regular directory with content

---
+++ Learn_ML/learn_machinelearning.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6c699af2c1764ef9be724f83e23cd2425ec98250214ae7402a58807a652c3d3
+size 897
---
+++ Learn_ML/train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2906a0f66940ad63feeb9109fb728a093e35dbea54050beeef3452af7a83eb0c
+size 6862
